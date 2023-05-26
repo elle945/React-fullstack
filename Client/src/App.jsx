3ad3e49ffe15558deb496e60c1a51390d973d44d
Venessa Karlsson <nessy.right@live.se>
2023-05-26 10:16:22 +0200

    fixade app.js

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,75 +1,24 @@
 import './App.css'
-import axios from 'axios'
-import {useState, useEffect} from 'react'
 import HomePage from './pages/Home'
 import PlacesPage from './pages/Places'
 import ReviewPage from './pages/Review'
 import NavBar from './components/Navbar'
 import Footer from './components/Footer'
-import ProductCards from './components/ProductCards'
-import UploadForm from './components/UploadForm'
 import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
-  const [data, setData] = useState([])
-  const [success, setSuccess] = useState(false)
-
-
-
-useEffect(() => {
-  axios.get('http://localhost:3000/persons')
-  .then(response => {
-      setData(response.data)
-  })
-.catch(() => {
-  //handle errors
-});
-}, []);
-
-//Post data
-const [formData, setFormData] = useState({
-  name: ''
-})
-
-const handleChange = (event) => {
-  setFormData({
-    ...formData,
-    [event.target.name] : event.target.value
-  })
-};
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  axios.post('http://localhost:3000/persons/submit-form', formData)
-  .then(() => {
-    // Hantera success på olika sätt
-    setSuccess(true);
-  })
-  .catch(() => {
-    //handle errors
-  });
-}
-
-
-
-
-
-
 
   return (
     <>
     <div>
       <NavBar/>
 
-
 <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/platser" element={<UploadForm />} />
-          <Route path="/recensioner" element={<ProductCards />} />
-
+          <Route path="/platser" element={<PlacesPage />} />
+          <Route path="/recensioner" element={<ReviewPage />} />
           </Routes>
     <Footer/>
-
       </div>
       </>
   )
