b592b32825354d4e3f51fdd448035fbcdfea662e
elle945 <eaj.nilsson@gmail.com>
2023-06-07 20:53:11 +0200

    link home

--- Client/src/pages/ErrorPage.jsx
+++ Client/src/pages/ErrorPage.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 function ErrorPage() {
   return (
     <>
     <div>
-            <p>There was an error loading this page. Please try again later.</p>
+            <p>There was an error loading this page.</p>
+            <Link to={"/"}>Go back to home</Link>
             </div>
     </>
   )
